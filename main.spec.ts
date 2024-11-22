94f28add201a781666d67907ec06db0e5428c537
Jannis Wingeyer <jannis@wingeyer.com>
2024-11-22 15:39:16 +0100

    fix

--- main.spec.ts
+++ main.spec.ts
@@ -1,52 +1,52 @@
 import { firstValueFrom } from "rxjs";
 import {
-  getLukeSkywalkerInfo,
-  getLukeSkywalkerInfoAsync,
-  getLukeSkywalkerInfoObservable,
-  PersonInfo,
+	getLukeSkywalkerInfo,
+	getLukeSkywalkerInfoAsync,
+	getLukeSkywalkerInfoObservable,
+	PersonInfo,
 } from "./main";
 
 const expectedResult: PersonInfo = {
-  name: "Luke Skywalker",
-  height: "172",
-  gender: "male",
-  homeworld: "Tatooine",
-  films: [
-    {
-      title: "A New Hope",
-      director: "George Lucas",
-      release_date: "1977-05-25",
-    },
-    {
-      title: "The Empire Strikes Back",
-      director: "Irvin Kershner",
-      release_date: "1980-05-17",
-    },
-    {
-      title: "Return of the Jedi",
-      director: "Richard Marquand",
-      release_date: "1983-05-25",
-    },
-    {
-      title: "Revenge of the Sith",
-      director: "George Lucas",
-      release_date: "2005-05-19",
-    },
-  ],
+	name: "Luke Skywalker",
+	height: 172,
+	gender: "male",
+	homeworld: "Tatooine",
+	films: [
+		{
+			title: "A New Hope",
+			director: "George Lucas",
+			release_date: "1977-05-25",
+		},
+		{
+			title: "The Empire Strikes Back",
+			director: "Irvin Kershner",
+			release_date: "1980-05-17",
+		},
+		{
+			title: "Return of the Jedi",
+			director: "Richard Marquand",
+			release_date: "1983-05-25",
+		},
+		{
+			title: "Revenge of the Sith",
+			director: "George Lucas",
+			release_date: "2005-05-19",
+		},
+	],
 };
 
 test("getLukeSkywalkerInfo", () => {
-  return getLukeSkywalkerInfo().then((result) => {
-    expect(result).toStrictEqual(expectedResult);
-  });
+	return getLukeSkywalkerInfo().then((result) => {
+		expect(result).toStrictEqual(expectedResult);
+	});
 });
 
 test("getLukeSkywalkerInfoAsync", async () => {
-  const result = await getLukeSkywalkerInfoAsync();
-  expect(result).toStrictEqual(expectedResult);
+	const result = await getLukeSkywalkerInfoAsync();
+	expect(result).toStrictEqual(expectedResult);
 });
 
 test("getLukeSkywalkerInfoObservable", async () => {
-  const result = await firstValueFrom(getLukeSkywalkerInfoObservable());
-  expect(result).toStrictEqual(expectedResult);
+	const result = await firstValueFrom(getLukeSkywalkerInfoObservable());
+	expect(result).toStrictEqual(expectedResult);
 });
--- main.ts
+++ main.ts
@@ -23,48 +23,109 @@ and dependent data from swapi to return the following object
 }
 
 Define an interface of the result type above and all other types as well.
-
 */
+
+interface Film {
+  title: string;
+  director: string;
+  release_date: string;
+}
+
+export interface PersonInfo {
+  name: string;
+  height: number;
+  gender: string;
+  homeworld: string;
+  films: Film[];
+}
 
 interface Person {
   name: string;
   height: string;
-  gender: "male" | "female" | "divers";
+  gender: string;
   homeworld: string;
   films: string[];
-}
-
-export interface PersonInfo {
-  // TODO: define type
 }
 
 // Task 1: write a function using promise based fetch api
 type PromiseBasedFunction = () => Promise<PersonInfo>;
 export const getLukeSkywalkerInfo: PromiseBasedFunction = () => {
-  return fetch("https://swapi.dev/api/people/1").then((response: Response) => {
-    return response.json().then((person: Person) => {
-      // TODO: load other stuff and return LukeSkywalkerInfo
-      return {} as PersonInfo;
+  return fetch("https://swapi.dev/api/people/1")
+    .then((response: Response) => response.json())
+    .then(async (person: Person) => {
+      const homeworldResponse = await fetch(person.homeworld);
+      const homeworldData = await homeworldResponse.json();
+
+      const filmPromises = person.films.map((filmUrl) =>
+        fetch(filmUrl).then((response) => response.json())
+      );
+      const filmsData = await Promise.all(filmPromises);
+
+      return {
+        name: person.name,
+        height: parseInt(person.height, 10),
+        gender: person.gender,
+        homeworld: homeworldData.name,
+        films: filmsData.map((film) => ({
+          title: film.title,
+          director: film.director,
+          release_date: film.release_date,
+        })),
+      };
     });
-  });
 };
 
 // Task 2: write a function using async and await
-// see also: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-1-7.html
 type AsyncBasedFunction = () => Promise<PersonInfo>;
-export const getLukeSkywalkerInfoAsync: PromiseBasedFunction = async () => {
+export const getLukeSkywalkerInfoAsync: AsyncBasedFunction = async () => {
   const response = await fetch("https://swapi.dev/api/people/1");
-  // TODO: load other stuff and return LukeSkywalkerInfo
-  return (await {}) as PersonInfo;
+  const person = await response.json();
+
+  const homeworldResponse = await fetch(person.homeworld);
+  const homeworldData = await homeworldResponse.json();
+
+  const filmPromises = person.films.map((filmUrl: string) =>
+    fetch(filmUrl).then((response) => response.json())
+  );
+  const filmsData = await Promise.all(filmPromises);
+
+  return {
+    name: person.name,
+    height: parseInt(person.height, 10),
+    gender: person.gender,
+    homeworld: homeworldData.name,
+    films: filmsData.map((film) => ({
+      title: film.title,
+      director: film.director,
+      release_date: film.release_date,
+    })),
+  };
 };
 
 // Task 3: write a function using Observable based api
 // see also: https://rxjs.dev/api/index/function/forkJoin
+import { forkJoin } from "rxjs";
+
 export const getLukeSkywalkerInfoObservable = () => {
   return get<Person>("https://swapi.dev/api/people/1").pipe(
     mergeMap((person: Person) => {
-      // TODO: load other stuff and return LukeSkywalkerInfo
-      return of({} as PersonInfo);
+      const homeworld$ = get<{ name: string }>(person.homeworld);
+      const films$ = forkJoin(person.films.map((filmUrl) => get<Film>(filmUrl)));
+
+      return forkJoin([homeworld$, films$]).pipe(
+        map(([homeworldData, filmsData]) => ({
+          name: person.name,
+          height: parseInt(person.height, 10),
+          gender: person.gender,
+          homeworld: homeworldData.name,
+          films: filmsData.map((film) => ({
+            title: film.title,
+            director: film.director,
+            release_date: film.release_date,
+          })),
+        }))
+      );
     })
   );
 };
+
